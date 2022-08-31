e8907d58d4a8ab81d215d6df3d6f0c2bcd0c7885
irissu0415 <ariel840615@gmail.com>
2022-08-31 17:56:45 +0800

    add call car function

--- app.py
+++ app.py
@@ -236,7 +236,22 @@ def getLocationConfirmMessage(title, latitude, longitude):
 
 
 def getCallCarMessage(data):
-    message = dict()
+    message = {
+                "type": "template",
+                "altText": "This is a buttons template",
+                "template": {
+                    "type": "buttons",
+                    "text": f"請選擇至{data["title"]}預約叫車時間",
+                    "actions":[
+                        {
+                        "type": "datetimepicker",
+                        "label": "預約",
+                        "data": json.dumps(data),
+                        "mode": "datetime"
+                        }
+                    ]
+                }
+            }
     return message
 
 
